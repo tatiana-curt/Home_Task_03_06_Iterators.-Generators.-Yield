a681e108913f1d3fd72e022e588e6547b0b5d9b1
tatiana.curt <tatiana.curt@gmail.com>
2019-06-10 11:41:37 +0300

    Second commit

---
+++ Home_Task_03_06_Iterators.Generators.Yield.py
@@ -0,0 +1,56 @@
+import json
+import hashlib
+import shutil
+
+
+class Countries_gen:
+    def __init__(self, path):
+        with open(path) as new:
+            self.data = json.load(new)
+            # print(self.data)
+            self.data = [countri['name']['common'] for countri in self.data]
+            self.i = -1
+            print(self.data)
+            print(len(self.data))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # print(self.data)
+        self.i += 1
+        if self.i < len(self.data):
+            with open('links.txt', 'a', encoding='utf-8') as f:
+                f.write('{} - https://en.wikipedia.org/wiki/{}\n'.format(self.data[self.i], self.data[self.i].replace(" ", "_")))
+                print('{} - https://en.wikipedia.org/wiki/{}'.format(self.data[self.i], self.data[self.i].replace(" ", "_")))
+        else:
+            raise StopIteration
+
+
+my_iter = Countries_gen('countries.json')
+for item in my_iter:
+    pass
+# ____________________________________________________Задание 2__________________________________________________
+shutil.copyfile(r'links.txt', r'copy_links.txt') #Создаю копию так как 2-ое задание выполняется по умолчанию на файле из первого задания "links.txt"
+
+def generator_md5(path='links.txt'):
+    i = 1
+    while i != 0:
+        with open(path, 'r') as f:
+            data = f.readline()
+            md5 = hashlib.md5(data.encode('utf-8')).hexdigest()
+            yield md5, data
+            lines = f.readlines()
+            if len(lines) == 0:
+                i=0
+            else:
+               with open(path, "w") as f:
+                   for line in lines:
+                       f.write(line)
+#
+my_gener = generator_md5()
+
+for md5_item, data_item in my_gener:
+    print(md5_item, data_item)
+    with open('md5.txt', 'a', encoding='utf-8') as f:
+        f.write('{} - {}\n'.format(md5_item, data_item))
